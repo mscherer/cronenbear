c698c726496ca7f90289a10f5b609f8a09cd26af
Michael Scherer <misc@redhat.com>
2025-07-11 23:45:49 +0200

    Add a class for claendar linked to religions

---
+++ src/religion_calendar.rs
@@ -0,0 +1,44 @@
+use crate::google_public_calendar::{GooglePublicCalendar, GooglePublicCalendarError};
+use icalendar::Calendar;
+
+// the only one that appear in the interface as far as I see
+#[derive(Debug)]
+pub enum ReligionCode {
+    Christianism,
+    Judaism,
+    Islamic,
+    OrthodoxChristianism,
+    Hinduism,
+}
+
+#[derive(Debug)]
+pub struct ReligionCalendar {
+    religion_code: ReligionCode,
+}
+
+impl ReligionCalendar {
+    pub fn new(code: ReligionCode) -> Self {
+        Self {
+            religion_code: code,
+        }
+    }
+}
+impl GooglePublicCalendar for ReligionCalendar {
+    fn get_google_id(&self) -> String {
+        let r = match self.religion_code {
+            ReligionCode::Christianism => "christian",
+            ReligionCode::Hinduism => "hinduism",
+            ReligionCode::Islamic => "islamic",
+            ReligionCode::Judaism => "judaism",
+            ReligionCode::OrthodoxChristianism => "orthodox_christianity",
+        };
+        r.to_string()
+    }
+}
+
+impl TryFrom<ReligionCalendar> for Calendar {
+    type Error = GooglePublicCalendarError;
+    fn try_from(c: ReligionCalendar) -> Result<Self, Self::Error> {
+        c.to_ical()
+    }
+}
