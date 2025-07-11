aa5c2b26d0d4983bf82dc0c4543ab1bb53749a26
Michael Scherer <misc@redhat.com>
2025-07-11 15:40:41 +0200

    Fetch calendar from the web

--- src/google_public_calendar.rs
+++ src/google_public_calendar.rs
@@ -1,13 +1,20 @@
 const GCAL_PREFIX: &str = "https://calendar.google.com/calendar/ical/";
 const GCAL_SUFFIX: &str = "/public/basic.ics";
 const LANGUAGE: &str = "en";
+use ureq;
 
 pub trait GooglePublicCalendar {
     fn get_google_id(&self) -> String;
 
     fn construct_calendar_url(&self) -> String {
         let id = self.get_google_id();
-        let name = format!("{}.{}#holiday@group.v.calendar.google.com", LANGUAGE, id);
+        // cannot use # directly, convert to %23
+        let name = format!("{}.{}%23holiday@group.v.calendar.google.com", LANGUAGE, id);
         format!("{}{}{}", GCAL_PREFIX, name, GCAL_SUFFIX)
+    }
+
+    fn fetch_calendar_web(&self) -> Result<String, ureq::Error> {
+        let url = self.construct_calendar_url();
+        ureq::get(url).call()?.body_mut().read_to_string()
     }
 }
