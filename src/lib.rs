78903aa04d8ea5c875f35fdf4c03e1e8b729aa58
Michael Scherer <misc@redhat.com>
2025-07-15 16:18:25 +0200

    Clean up with rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
+pub mod aliases;
 pub mod country_calendar;
 pub mod google_public_calendar;
+pub mod index_page;
 pub mod merged_calendar;
 pub mod religion_calendar;
-pub mod aliases;
-pub mod index_page;
-
