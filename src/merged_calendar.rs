d0f8604136b16a4923e756f4720d8d571c5cb4fd
Michael Scherer <misc@redhat.com>
2025-07-18 14:11:42 +0200

    Add support for formatting

--- src/merged_calendar.rs
+++ src/merged_calendar.rs
@@ -2,11 +2,51 @@ use crate::aliases::FormatString;
 use crate::google_public_calendar::GooglePublicCalendar;
 use icalendar::CalendarComponent::Event;
 use icalendar::Component;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct MergedCalendar {
     events: Vec<icalendar::Event>,
     name: String,
+}
+
+fn format_summary(
+    summary: &str,
+    format: &FormatString,
+    format_hashmap: &HashMap<String, String>,
+) -> String {
+    let mut res = String::new();
+    let mut var = String::new();
+    let mut in_format = false;
+
+    for c in format.chars() {
+        if in_format {
+            if c == '}' {
+                in_format = false;
+                let v = if var == "summary" {
+                    summary.to_owned()
+                } else {
+                    format_hashmap
+                        .get(&var)
+                        .unwrap_or(&format!("{{{var}}}"))
+                        .to_string()
+                };
+                res.push_str(&v);
+                var.clear();
+            } else {
+                var.push(c);
+            }
+        } else if c == '{' {
+            in_format = true;
+        } else {
+            res.push(c);
+        }
+    }
+    if in_format {
+        res.push('{');
+        res.push_str(&var);
+    }
+    res
 }
 
 impl MergedCalendar {
@@ -19,6 +59,7 @@ impl MergedCalendar {
 
     // TODO remove the unwrap
     pub fn add<T: GooglePublicCalendar>(&mut self, calendar: &T, format: &Option<FormatString>) {
+        let format_hm = calendar.get_formatting_hashmap();
         for e in &calendar.to_ical().unwrap().components {
             if let Event(event) = e {
                 // TODO make sure this work if language is set to something else than english
@@ -29,9 +70,8 @@ impl MergedCalendar {
                     let mut e = event.clone();
                     let summary = String::from(e.get_summary().unwrap_or("Public holiday"));
 
-                    let s = if let Some(_f) = format {
-                        // TODO add some formating
-                        summary
+                    let s = if let Some(f) = format {
+                        format_summary(&summary, f, &format_hm)
                     } else {
                         format!("{}: {}", calendar.get_short_name(), summary)
                     };
@@ -67,5 +107,23 @@ mod test {
     fn test_add() {
         let m = MergedCalendar::new("test");
         assert_eq!(m.get_name(), "test");
+    }
+
+    #[test]
+    fn test_format_summary() {
+        let mut format_hm = HashMap::new();
+        format_hm.insert(
+            String::from("name"),
+            String::from("Sir Lancelot of Camelot"),
+        );
+        format_hm.insert(String::from("color"), String::from("blue"));
+
+        let summary = String::from("maybe more");
+        let format = FormatString::new("test: {name} like {color} and {summary}");
+        let res = format_summary(&summary, &format, &format_hm);
+        assert_eq!(
+            res,
+            "test: Sir Lancelot of Camelot like blue and maybe more"
+        );
     }
 }
