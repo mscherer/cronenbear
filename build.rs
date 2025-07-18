3db5afbed1ba85845baf17fbec9c2c76235d2019
Michael Scherer <misc@redhat.com>
2025-07-18 17:46:12 +0200

    Add a footer with git rev and build date

---
+++ build.rs
@@ -0,0 +1,34 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+use std::process::Command;
+
+fn main() {
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("const_build.rs");
+    let now = match Command::new("date").arg("-u").arg("+%Y-%m-%d").output() {
+        Ok(n) => String::from_utf8(n.stdout).unwrap(),
+        Err(_) => String::from("undefined"),
+    };
+
+    let git = match Command::new("git")
+        .arg("rev-parse")
+        .arg("--short")
+        .arg("HEAD")
+        .output()
+    {
+        Ok(u) => String::from_utf8(u.stdout).unwrap(),
+        Err(_) => String::from("HEAD"),
+    };
+
+    fs::write(
+        &dest_path,
+        format!(
+            "pub const DATE :&str = \"{now}\";
+         pub const GIT_REV :&str = \"{git}\";
+        "
+        ),
+    )
+    .unwrap();
+    println!("cargo::rerun-if-changed=build.rs");
+}
--- src/index_page.rs
+++ src/index_page.rs
@@ -1,5 +1,6 @@
 use crate::aliases::AliasID;
 use crate::aliases::AliasName;
+use crate::consts::{DATE, GIT_REV};
 use askama::Template;
 use std::collections::HashMap;
 
@@ -7,10 +8,16 @@ use std::collections::HashMap;
 #[template(path = "index.html")]
 pub struct IndexTemplate {
     calendars: HashMap<AliasID, AliasName>,
+    date: String,
+    git_rev: String,
 }
 
 impl IndexTemplate {
     pub fn new(calendars: HashMap<AliasID, AliasName>) -> Self {
-        Self { calendars }
+        Self {
+            calendars,
+            date: String::from(DATE),
+            git_rev: String::from(GIT_REV),
+        }
     }
 }
